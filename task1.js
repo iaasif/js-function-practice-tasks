5333f3a07596566297ea8e3b347720f5bbb76353
iaasif <imtiazasif.cse@gmail.com>
2024-02-04 01:52:10 +0600

    task2 done

---
+++ task1.js
@@ -0,0 +1,4 @@
+function multiply(p1, p2, p3, p4) {
+    return p1 * p2 * p3 * p4;
+}
+console.log(multiply(2, 3, 2, 1));
---
+++ task2.js
@@ -0,0 +1,10 @@
+function evenOdd(x) {
+    if (x % 2 == 0) {
+        return x / 2;
+    }
+    else {
+        return x * 2;
+    }
+}
+
+console.log(evenOdd(11));
